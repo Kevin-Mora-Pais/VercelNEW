46df5438111848dad3ae069e4165b8a31cc3c995
Kevin Mora Pais <kevin.mora@globant.com>
2020-12-03 15:00:32 -0300

    CORS

--- api/login.js
+++ api/login.js
@@ -1,7 +1,8 @@
 import { connectToDatabase } from '../lib/database'
 import { allowCors } from './cors'
 const cors = require('cors')
-allowCors (
+allowCors 
+
 module.exports = async (req, res) => {
     let userSearch
 
@@ -47,5 +48,26 @@ module.exports = async (req, res) => {
         }
     }
 
+    const allowCors = fn => async (req, res) => {
+        res.setHeader('Access-Control-Allow-Credentials', true)
+        res.setHeader('Access-Control-Allow-Origin', '*')
+        res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+        res.setHeader(
+            'Access-Control-Allow-Headers',
+            'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+        )
+        if (req.method === 'OPTIONS') {
+            res.status(200).end()
+            return
+        }
+        return await fn(req, res)
+    }
 
-})
+    const handler = (req, res) => {
+        const d = new Date()
+        res.end(d.toString())
+    }
+
+    module.exports = allowCors(handler)
+
+}
